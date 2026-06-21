ec455c259b029e69a301245f508895f0a7bf11f5
P Ankitha <your-github-email@example.com>
2026-06-21 17:56:54 +0530

    fix backend and clean deployment files

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ import string
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
-from models import db, User, Doctor, Patient, Appointment, Consultation, BlockedSlot
+from backend.models import db, User, Doctor, Patient, Appointment, Consultation, BlockedSlot
 from werkzeug.security import generate_password_hash, check_password_hash
  
 from sqlalchemy import inspect, text
